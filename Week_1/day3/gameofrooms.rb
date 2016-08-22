584606abf03c6a48afb51c0c5e2475d3b5b5e21a
jason <jasonok@protonmail.com>
2016-08-22 19:28:27 +0200

    Game of Rooms fully finished

--- Week_1/day3/gameofrooms.rb
+++ Week_1/day3/gameofrooms.rb
@@ -5,7 +5,7 @@ class Game
     @rooms = rooms
     @player = Player.new
     @load_bool = 0
-    puts "\n\n__xxx***You are trying to escape a maze of rooms.***xxx__\n\nEach room has up to 4 possible exits:\nNorth: 'N'\nSouth: 'S'\nEast: 'E'\nWest: 'W'\nExit the game: 'Q'\nCurrent room's info: 'room?'\nHint for current room: 'hint?\nCheck room: 'look around'\nPick up item in the room: 'get item'\nCheck inventory: 'inventory?'\nSet down item in room: 'drop item'"
+    puts "\n\n__xxx***ESCAPE THIS MAZE OF ROOMS.***xxx__\n\nEach room has up to 4 possible exits:\nNorth: 'N'\nSouth: 'S'\nEast: 'E'\nWest: 'W'\nExit the game: 'Q'\nCurrent room's info: 'room?'\nHint for current room: 'hint?\nCheck room: 'look around'\nPick up item in the room: 'get item'\nCheck inventory: 'inventory?'\nSet down item in room: 'drop item'"
   end
 
   def save_game
@@ -21,11 +21,9 @@ class Game
     @player.set_location(@rooms[@load_game[0].chomp.to_sym])
     @player.inventory = @load_game[1].chomp.gsub(/(\[\"|\"\])/, '').split('", "') #strips out unnecessary characters and converts to array
     @rooms = eval(@load_game[2].chomp) #converts the string to a hash
-    binding.pry
   end
 
   def start_game
-    # @rooms = rooms
     print "\nEnter any command to begin\n"
     @player.set_location(@rooms[:jailcell]) # : @player.set_location(@rooms[@load_game[0].chomp.to_sym]) #puts player into the  jailcell as starting room
       while @char != "Q"
@@ -45,7 +43,7 @@ class Game
         when "HINT?"
           puts @player.get_hint
         when "INVENTORY?"
-          puts "You have #{@player.check_inventory.join(', ')} on your person"
+          puts "You have #{@player.check_inventory.join(', ')} in your inventory"
         when "EXITS"
           puts @player.print_exits
         when "SAVE GAME"
@@ -53,17 +51,9 @@ class Game
         when "LOAD GAME"
           load_game
         when "LOOK AROUND"
-          if @player.find_items.empty?
-            puts "The room is empty"
-          else
-            puts "You see #{@player.find_items.join('')} in the room"
-          end
+          @player.find_items.empty? ? (puts "The room is empty") : (puts "You see #{@player.find_items.join(', ')} in the room")
         when "GET ITEM"
-          if @player.find_items.empty?
-            puts "Thare aint nuthin ta git!"
-          else
-            @player.pick_up_item
-          end
+          @player.find_items.empty? ? (puts "Thare aint nuthin ta git!") : @player.pick_up_item
         when "DROP ITEM"
           puts "Which item do you want to drop?"
           item = gets.chomp.upcase
@@ -124,11 +114,7 @@ class Player
   end
 
   def find_items #returns an array
-    if @location[:objects]
-      @location[:objects].each {|x| x}
-    else
-      false
-    end
+    @location[:objects] ? @location[:objects].each {|x| x} : false
   end
 
   def pick_up_item
@@ -140,8 +126,7 @@ class Player
   end
 
   def put_down_item(thing)
-    item_to_drop = @inventory.select {|object| object==thing} #assigns item to drop to be 'thing' in the inventory array
-    item_to_drop = item_to_drop.join('') #converts array to a string
+    item_to_drop = @inventory.select {|object| object==thing}.join('') #assigns item to drop to be 'thing' in the inventory array
     @inventory.delete_if { |item| item==thing} #deletes item from inventory
     @location[:objects] << item_to_drop #puts item into :objects has of the room
   end
@@ -152,8 +137,8 @@ rooms = {
   :jailcell => {:title => "jailcell",:exits=>{"S" => 'main_hallway'},:objects => ["KEYS"],:hint=>"go South, foolio",:desc=>"You just woke up from a beer drinking contest. You appear to be in an old jail but no one else is here. It appears someone left the south-facing door open..."},
   :main_hallway => {:title=>"main_hallway",:exits=>{"N" => 'jailcell', "E" => 'torture_chamber', "S"=>'yard'},:objects=>[],:hint=>"Just because the hall is dark that doesn't mean it's scary",:desc=>"The hallway is dark, except for the old flickering lights in one corner and light shining through a door to your left. You could keep going South but it is too dark to see what's at the end of the hall"},
   :torture_chamber => {:title=>"torture_chamber",:exits=>{"W"=>'main_hallway'},:objects =>["SAW"],:hint=>"I wonder what Alexa has seen and heard",:desc=>"A modern day torture chamber! With smart lighting to save on the electric bill and torture commands linked to Alexa"},
-  :yard => {:title=>"yard", :exits => {"N"=>'main_hallway',"E" => 'yard2',"S"=>'garage',"W"=>'office'},:objects=>[],:hint=>"This courtyard is open, you can go in any diretion",:desc=>"You step outside into a sunny, grassy courtyard. All sides are either fenced in or bordering buildings."},
-  :small_yard => {:title=>"Small_yard",:exits=>{"W"=>'yard'},:objects=>[],:hint=>"It's the door you came in! Which unfortunately is the only interesting thing in here",:desc=>"You go through a gate in the main courtyard's fence and end up in a smaller enclosed dirt yard. What's that over there!?"}
+  :yard => {:title=>"yard", :exits => {"N"=>'main_hallway',"E" => 'small_yard',"S"=>'garage',"W"=>'office'},:objects=>[],:hint=>"This courtyard is open, you can go in any diretion",:desc=>"You step outside into a sunny, grassy courtyard. All sides are either fenced in or bordering buildings."},
+  :small_yard => {:title=>"small_yard",:exits=>{"W"=>'yard'},:objects=>[],:hint=>"It's the door you came in! Which unfortunately is the only interesting thing in here",:desc=>"You go through a gate in the main courtyard's fence and end up in a smaller enclosed dirt yard. What's that over there!?"}
 }
 
 mygame=Game.new(rooms)
--- Week_2/sinatra_spec.rb
+++ Week_2/sinatra_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'sinatraTODO.rb'
+require 'pry'
 
 RSpec.describe Task do
   before :each do
@@ -17,7 +18,79 @@ describe "#complete!" do
   end
 end
 
+describe "#completed attr" do
+  it "checks that a new time variable was created and the time is different" do
+    expect(@task.created_at<=>Time.now).to eq((-1))
+  end
+end
 
+describe "#completed attr" do
+  it "checks that a new time variable was created and the day is same" do
+    expect((@task.created_at).day==Time.now.day).to eq((true))
+  end
+end
 
-
+describe "#update_content!" do
+  it "changes a tasks content" do
+    expect(@task.update_content!("new content")).to eq(("new content"))
+  end
+end
 end#end class
+
+
+
+RSpec.describe TodoList do
+  before :each do
+  @list = TodoList.new
+  @singletask = Task.new("pass this test")
+  @singletask2 = Task.new("i like cats")
+  @singletask3 = Task.new("this should be last")
+end
+
+describe "#add_task" do
+  it "adds single task to our array of tasks" do
+    @list.add_task(@singletask)
+    @list.add_task(@singletask2)
+    expect(@list.tasks.size).to eq(2)
+  end
+end
+
+describe "#delete_task" do
+  it "deletes task from task array by its ID. first tests that task is actually added, then deleted" do
+    a = @list.add_task(@singletask).size
+    @list.delete_task(@singletask.id)
+    expect(a == 1 && @list.tasks.empty?).to eq(true)
+  end
+end
+
+describe "#find_task_by_id" do
+  it "searches the tasks array and returns the one with matching IDs" do
+    @list.add_task(@singletask)
+    @list.add_task(@singletask2)
+    search_id = @singletask2.id
+    expect(@list.find_task_by_id(search_id)).to eq("i like cats")
+  end
+end
+
+describe "#find_task_by_id nil when array empty" do
+  it "should return nil" do
+    search_id = @singletask.id
+    expect(@list.find_task_by_id(search_id)).to eq(nil)
+  end
+end
+
+describe "#sort_by_created" do
+  it "sort tasks based on time created" do
+    binding.pry
+    task1 = Task.new("fuck")
+    task2 = Task.new("dumb")
+    task3 = Task.new("STUPID")
+    @list.add_task(task3)
+    @list.add_task(task1)
+    @list.add_task(task2)
+    # new_arr = @list.sort_by_created
+    expect(@list.size).to eq(3)
+  end
+end
+
+end
