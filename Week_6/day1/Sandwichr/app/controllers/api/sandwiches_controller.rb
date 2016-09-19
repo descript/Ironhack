f247097b49aed68fcc1fd52cad129a03504d78c3
jason <jasonok@protonmail.com>
2016-09-19 10:30:22 +0200

    namespace api added

---
+++ Week_6/day1/Sandwichr/app/controllers/api/sandwiches_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  class SandwichesController < ApplicationController
+    def index
+      sandwiches = Sandwich.all
+      render json: sandwiches
+    end
+
+    def show
+      sandwiches = Sandwich.find(params[:id])
+      render json: sandwiches
+    end
+  end
+end
